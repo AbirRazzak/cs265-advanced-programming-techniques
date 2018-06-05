a087892e7fc5d24c08fa4d3dc9bda9f12e9590cd
Abir Razzak <amr439@drexel.edu>
2018-06-05 12:17:50 -0400

    Fixed init logic

--- assn3/assn4.py
+++ assn3/assn4.py
@@ -16,10 +16,6 @@ class Register:
     TWENTIES_VALUE = 20
 
     def __init__ (self, amt, ones, fives=0, tens=0, twenties=0) :
-        '''Immediately exit with code 1 if any of the args are negative'''
-        if(amt < 0 or ones < 0 or fives < 0 or tens < 0 or twenties < 0) :
-            sys.exit(1)
-        
         try:
             self.sales = 0
             self.total = int(amt)
@@ -27,6 +23,9 @@ class Register:
             self.fives = int(fives)
             self.tens = int(tens)
             self.twenties = int(twenties)
+            '''Exit with code 1 if any of the args are negative'''
+            if(amt < 0 or ones < 0 or fives < 0 or tens < 0 or twenties < 0) :
+                sys.exit(1)
         except ValueError:
             '''String that is passed in does not represent an integer'''
             sys.exit(1)
